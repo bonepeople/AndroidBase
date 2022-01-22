52bdb67600fc6a33eb3524910bc59d9160e674cd
bonepeople <bonepeople@sina.com>
2022-01-22 22:31:26 +0800

    添加SimpleRecyclerViewAdapter

---
+++ base/src/main/java/com/bonepeople/android/base/example/SimpleRecyclerViewAdapter.kt
@@ -0,0 +1,30 @@
+package com.bonepeople.android.base.example
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bonepeople.android.base.databinding.ItemSimpleListBinding
+
+class SimpleRecyclerViewAdapter(private val list: ArrayList<Any>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return DataHolder(ItemSimpleListBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is DataHolder -> {
+                holder.binding.let { views ->
+                    val data = list[position]
+                    //...
+                    views.root.setOnClickListener {
+                        //...
+                    }
+                }
+            }
+        }
+    }
+
+    override fun getItemCount() = list.size
+
+    private class DataHolder(val binding: ItemSimpleListBinding) : RecyclerView.ViewHolder(binding.root)
+}
