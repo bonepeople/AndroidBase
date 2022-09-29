9b4b71938478dace2ef8c2527f7b047cc10181e2
bonepeople <bonepeople@sina.com>
2022-09-29 10:25:34 +0800

    修复CustomLoadingDialog无法使用的问题
    忘记添加views.root导致DialogCustomLoadingBinding在混淆过程中被移除

--- base/src/main/java/com/bonepeople/android/base/view/CustomLoadingDialog.kt
+++ base/src/main/java/com/bonepeople/android/base/view/CustomLoadingDialog.kt
@@ -7,6 +7,7 @@ import com.bonepeople.android.base.databinding.DialogCustomLoadingBinding
 class CustomLoadingDialog(manager: FragmentManager) : ViewBindingDialogFragment<DialogCustomLoadingBinding>(manager) {
 
     override fun initView() {
+        views.root
         isCancelable = false
     }
 }
