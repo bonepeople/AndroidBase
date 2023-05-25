355d1420c78624eea4ce4e783f9118835371659a
bonepeople <bonepeople@sina.com>
2023-05-25 23:14:22 +0800

    更新AndroidWidget版本
    依赖库添加对于多进程的支持
    修复获取电池电量时读数不变的问题
    为DateTimePicker添加取消按钮
    AppPermission添加check方法，用于检查权限授予情况

--- base/src/main/java/com/bonepeople/android/base/manager/BaseApp.kt
+++ base/src/main/java/com/bonepeople/android/base/manager/BaseApp.kt
@@ -1,6 +1,7 @@
 package com.bonepeople.android.base.manager
 
 import android.app.Application
+import androidx.startup.StartupHelper
 import com.bonepeople.android.widget.util.AppStorage
 
 abstract class BaseApp : Application() {
@@ -8,6 +9,7 @@ abstract class BaseApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        StartupHelper.initializeAll(this)
         AppStorage.putString("com.bonepeople.android.key.APP_NAME", appName)
     }
 }
