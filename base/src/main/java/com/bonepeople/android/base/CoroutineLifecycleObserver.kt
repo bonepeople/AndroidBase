8aecd4a966851b54651f948f67a006887319fd9e
bonepeople <bonepeople@sina.com>
2023-08-02 23:52:17 +0800

    使用LifecycleEventObserver替换注解版的生命周期回调

--- base/src/main/java/com/bonepeople/android/base/CoroutineLifecycleObserver.kt
+++ base/src/main/java/com/bonepeople/android/base/CoroutineLifecycleObserver.kt
@@ -1,18 +1,17 @@
 package com.bonepeople.android.base
 
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.OnLifecycleEvent
 import kotlinx.coroutines.cancel
 import kotlin.coroutines.CoroutineContext
 
-class CoroutineLifecycleObserver(private val coroutineContext: CoroutineContext) : LifecycleObserver {
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    fun cancelCoroutine(lifecycleOwner: LifecycleOwner) {
-        coroutineContext.cancel()
-        lifecycleOwner.lifecycle.removeObserver(this)
+class CoroutineLifecycleObserver(private val coroutineContext: CoroutineContext) : LifecycleEventObserver {
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        if (event == Lifecycle.Event.ON_DESTROY) {
+            coroutineContext.cancel()
+            source.lifecycle.removeObserver(this)
+        }
     }
 
     override fun hashCode(): Int {
