a69aae523eeb185798cb42e73e72e52a66b12d86
bonepeople <bonepeople@sina.com>
2023-10-18 22:07:05 +0800

    修改observeWithLifecycle函数，添加distinctUntilChanged方法
    StateFlow在数据未变更的时候不会重复通知收集器，但使用flowWithLifecycle函数后会转换为Flow，界面会在从后台切回前台时收到回调，使用distinctUntilChanged可以避免此情况

--- base/src/main/java/com/bonepeople/android/base/util/FlowExtension.kt
+++ base/src/main/java/com/bonepeople/android/base/util/FlowExtension.kt
@@ -7,15 +7,12 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 
 @Suppress("UNUSED")
 object FlowExtension {
     fun <T> Flow<T>.observeWithLifecycle(owner: LifecycleOwner, activeState: Lifecycle.State = Lifecycle.State.STARTED, action: suspend (T) -> Unit): Job {
-        return flowWithLifecycle(owner.lifecycle, activeState).onEach(action).launchIn(owner.lifecycleScope)
+        return flowWithLifecycle(owner.lifecycle, activeState).distinctUntilChanged().onEach(action).launchIn(owner.lifecycleScope)
     }
 
     fun MutableStateFlow<String>.bindEditTextValue(owner: LifecycleOwner, editText: EditText) {
