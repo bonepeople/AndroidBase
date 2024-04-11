34fb10dc366e87948034415388b5a58525e702f8
bonepeople <bonepeople@qq.com>
2024-04-11 11:58:16 +0800

    add build version

--- base/src/main/java/com/bonepeople/android/base/manager/BaseApp.kt
+++ base/src/main/java/com/bonepeople/android/base/manager/BaseApp.kt
@@ -3,13 +3,16 @@ package com.bonepeople.android.base.manager
 import android.app.Application
 import androidx.startup.StartupHelper
 import com.bonepeople.android.widget.util.AppData
+import com.bonepeople.android.widget.util.AppLog
 
 abstract class BaseApp : Application() {
+    private val build = "20240411-115732"
     protected abstract val appName: String
 
     override fun onCreate() {
         super.onCreate()
         StartupHelper.initializeAll(this)
         AppData.default.putStringSync("com.bonepeople.android.key.APP_NAME", appName)
+        AppLog.defaultLog.verbose("build: $build")
     }
 }
