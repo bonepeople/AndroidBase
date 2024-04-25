507dda78f4ef440c683fddb0ec7eece889fbbfa7
bonepeople <bonepeople@sina.com>
2024-04-25 23:40:55 +0800

    Update AndroidWidget and adjust code

--- base/src/main/java/com/bonepeople/android/base/manager/BaseApp.kt
+++ base/src/main/java/com/bonepeople/android/base/manager/BaseApp.kt
@@ -2,7 +2,6 @@ package com.bonepeople.android.base.manager
 
 import android.app.Application
 import androidx.startup.StartupHelper
-import com.bonepeople.android.widget.util.AppStorage
 
 abstract class BaseApp : Application() {
     protected abstract val appName: String
@@ -10,6 +9,5 @@ abstract class BaseApp : Application() {
     override fun onCreate() {
         super.onCreate()
         StartupHelper.initializeAll(this)
-        AppStorage.putString("com.bonepeople.android.key.APP_NAME", appName)
     }
 }
--- base/src/main/java/com/bonepeople/android/base/manager/BaseUserManager.kt
+++ base/src/main/java/com/bonepeople/android/base/manager/BaseUserManager.kt
@@ -1,6 +1,7 @@
 package com.bonepeople.android.base.manager
 
 import com.bonepeople.android.localbroadcastutil.LocalBroadcastUtil
+import com.bonepeople.android.widget.util.AppData
 import com.bonepeople.android.widget.util.AppGson
 import com.bonepeople.android.widget.util.AppStorage
 import java.lang.reflect.ParameterizedType
@@ -9,21 +10,37 @@ import java.lang.reflect.ParameterizedType
 abstract class BaseUserManager<D> {
     private val userClass: Class<*> = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0] as Class<*>
     private val defaultUserInfo: D = userClass.newInstance() as D
+    private val userData = AppData.create("com.bonepeople.android.base.manager.BaseUserManager")
+
+    @Deprecated("Since AppStorage has been deprecated, replace its usage with AppData. This property will be removed from version 1.7.0.")
+    private val userStorage = AppStorage
     var token: String = USER_TOKEN
         get() {
             if (field == USER_TOKEN) {
-                field = AppStorage.getString(USER_TOKEN)
+                field = userData.getStringSync(USER_TOKEN).ifEmpty {
+                    val data = userStorage.getString(USER_TOKEN)
+                    userData.putStringSync(USER_TOKEN, data)
+                    data
+                }
             }
             return field
         }
         set(value) {
-            AppStorage.putString(USER_TOKEN, value)
+            userData.putStringSync(USER_TOKEN, value)
             field = value
         }
     val userId: String
-        get() = AppStorage.getString(USER_ID)
+        get() = userData.getStringSync(USER_ID).ifEmpty {
+            val data = userStorage.getString(USER_ID)
+            userData.putStringSync(USER_ID, data)
+            data
+        }
     val userInfo: D
-        get() = AppGson.defaultGson.fromJson<D>(AppStorage.getString(USER_INFO), userClass) ?: defaultUserInfo
+        get() = AppGson.defaultGson.fromJson<D>(userData.getStringSync(USER_INFO).ifEmpty {
+            val data = userStorage.getString(USER_INFO)
+            userData.putStringSync(USER_INFO, data)
+            data
+        }, userClass) ?: defaultUserInfo
 
     val isLogin: Boolean
         get() {
@@ -32,8 +49,8 @@ abstract class BaseUserManager<D> {
 
     fun login(newInfo: D) {
         val login = !isLogin
-        AppStorage.putString(USER_INFO, AppGson.toJson(newInfo))
-        AppStorage.putString(USER_ID, resolveUserId(newInfo))
+        userData.putStringSync(USER_INFO, AppGson.toJson(newInfo))
+        userData.putStringSync(USER_ID, resolveUserId(newInfo))
         if (login) {
             LocalBroadcastUtil.sendBroadcast(BroadcastAction.USER_LOGIN)
         } else {
@@ -44,8 +61,8 @@ abstract class BaseUserManager<D> {
     fun logout() {
         if (isLogin) {
             token = ""
-            AppStorage.putString(USER_INFO, "")
-            AppStorage.putString(USER_ID, resolveUserId(defaultUserInfo))
+            userData.putStringSync(USER_INFO, "")
+            userData.putStringSync(USER_ID, resolveUserId(defaultUserInfo))
             LocalBroadcastUtil.sendBroadcast(BroadcastAction.USER_LOGOUT)
         }
     }
