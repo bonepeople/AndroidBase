e0daa6d74c742719cc7814e0ed110f2f41dd7c97
bonepeople <bonepeople@sina.com>
2024-09-20 22:31:19 +0800

    Update AndroidWidget and adjust code

--- base/src/main/java/com/bonepeople/android/base/manager/BaseUserManager.kt
+++ base/src/main/java/com/bonepeople/android/base/manager/BaseUserManager.kt
@@ -1,43 +1,16 @@
 package com.bonepeople.android.base.manager
 
-import androidx.shade.migrate.DataMigrateInfo
-import androidx.shade.migrate.DataMigrateUtil
-import com.bonepeople.android.base.util.CoroutineExtension.launchOnIO
 import com.bonepeople.android.localbroadcastutil.LocalBroadcastUtil
 import com.bonepeople.android.widget.util.AppData
 import com.bonepeople.android.widget.util.AppGson
-import com.bonepeople.android.widget.util.AppStorage
-import kotlinx.coroutines.runBlocking
 import java.lang.reflect.ParameterizedType
 
 @Suppress("UNCHECKED_CAST")
 abstract class BaseUserManager<D> {
     private val userClass: Class<*> = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0] as Class<*>
     private val defaultUserInfo: D = userClass.newInstance() as D
-    private val userData by lazy {
-        val field = AppData.create("com.bonepeople.android.base.manager.BaseUserManager")
-        runBlocking {
-            DataMigrateUtil.migrate(
-                dataId = "com.bonepeople.android.base.manager.BaseUserManager",
-                migrateList = listOf(
-                    object : DataMigrateInfo {
-                        override val range: IntRange = 0..1
-                        override val action: suspend () -> Unit = {
-                            field.putString(USER_TOKEN, AppStorage.getString(USER_TOKEN))
-                            field.putString(USER_ID, AppStorage.getString(USER_ID))
-                            field.putString(USER_INFO, AppStorage.getString(USER_INFO))
-                            launchOnIO {
-                                AppStorage.remove(USER_TOKEN)
-                                AppStorage.remove(USER_ID)
-                                AppStorage.remove(USER_INFO)
-                            }
-                        }
-                    },
-                )
-            )
-        }
-        field
-    }
+    private val userData: AppData = AppData.create("com.bonepeople.android.base.manager.BaseUserManager")
+
     var token: String = USER_TOKEN
         get() {
             if (field == USER_TOKEN) {
