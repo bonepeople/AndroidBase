55ff7f497568289a1a1913166a3f1cfb4f067250
bonepeople <bonepeople@sina.com>
2024-11-27 20:14:50 +0800

    Store `appName` asynchronously during app startup

--- base/src/main/java/com/bonepeople/android/base/manager/BaseApp.kt
+++ base/src/main/java/com/bonepeople/android/base/manager/BaseApp.kt
@@ -2,12 +2,18 @@ package com.bonepeople.android.base.manager
 
 import android.app.Application
 import androidx.startup.StartupHelper
+import com.bonepeople.android.widget.CoroutinesHolder
+import com.bonepeople.android.widget.util.AppData
+import kotlinx.coroutines.launch
 
 abstract class BaseApp : Application() {
     protected abstract val appName: String
 
     override fun onCreate() {
         super.onCreate()
+        CoroutinesHolder.io.launch {
+            AppData.create("com.bonepeople.android.base.manager.BaseApp").putString("appName", appName)
+        }
         StartupHelper.initializeAll(this)
     }
 }
