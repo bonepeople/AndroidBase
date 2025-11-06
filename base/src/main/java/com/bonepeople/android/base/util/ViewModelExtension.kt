cf1909e3c9cf3db11f56d77df80a92bdb4f1a9cb
bonepeople <bonepeople@sina.com>
2025-11-06 22:58:45 +0800

    Add initializeOnce extension for ViewModel

--- base/src/main/java/com/bonepeople/android/base/util/ViewModelExtension.kt
+++ base/src/main/java/com/bonepeople/android/base/util/ViewModelExtension.kt
@@ -6,19 +6,32 @@ import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelStorage.getExtra
 import androidx.lifecycle.ViewModelStorage.putExtraIfAbsent
+import androidx.lifecycle.viewModelScope
+import com.bonepeople.android.base.util.CoroutineExtension.launchOnDefault
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import java.io.Closeable
+import java.util.concurrent.atomic.AtomicBoolean
 
 @Suppress("Unused")
 object ViewModelExtension {
     private const val LIFECYCLE_KEY = "com.bonepeople.android.base.util.ViewModelExtension.LIFECYCLE_KEY"
+    private const val INITIALIZE_ONCE_KEY = "com.bonepeople.android.base.util.ViewModelExtension.INITIALIZE_ONCE_KEY"
 
     val ViewModel.lifecycleOwner: LifecycleOwner
         get() {
             val lifecycleOwner: LifecycleOwner? = getExtra(LIFECYCLE_KEY)
             return lifecycleOwner ?: putExtraIfAbsent(LIFECYCLE_KEY, LifecycleOwnerHolder())
         }
+
+    fun ViewModel.initializeOnce(block: suspend () -> Unit) {
+        val initialized = putExtraIfAbsent(key = INITIALIZE_ONCE_KEY, value = AtomicBoolean(false))
+        if (!initialized.compareAndSet(false, true)) return
+
+        viewModelScope.launchOnDefault {
+            block()
+        }
+    }
 
     private class LifecycleOwnerHolder : LifecycleOwner, Closeable {
         private val registry = LifecycleRegistry(this)
--- sample/src/main/java/com/bonepeople/android/base/sample/module/start/StartViewModel.kt
+++ sample/src/main/java/com/bonepeople/android/base/sample/module/start/StartViewModel.kt
@@ -1,36 +1,30 @@
 package com.bonepeople.android.base.sample.module.start
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.bonepeople.android.base.sample.global.UserManager
-import com.bonepeople.android.base.util.CoroutineExtension.launchOnDefault
+import com.bonepeople.android.base.util.ViewModelExtension.initializeOnce
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import java.util.concurrent.atomic.AtomicBoolean
 
 class StartViewModel : ViewModel() {
     val pageState: MutableStateFlow<PageState> = MutableStateFlow(PageState.Init)
-    private val initialized = AtomicBoolean(false)
 
-    fun init() {
-        if (!initialized.compareAndSet(false, true)) return
-        viewModelScope.launchOnDefault {
-            runCatching {
-                pageState.value = PageState.Loading
-                coroutineScope {
-                    launch {
-                        delay(2000)
-                    }
-                    launch {
-                        UserManager.isLogin
-                    }
+    fun init() = initializeOnce {
+        runCatching {
+            pageState.value = PageState.Loading
+            coroutineScope {
+                launch {
+                    delay(2000)
                 }
-                pageState.value = PageState.Finish
-            }.onFailure {
-                pageState.value = PageState.Error
+                launch {
+                    UserManager.isLogin
+                }
             }
+            pageState.value = PageState.Finish
+        }.onFailure {
+            pageState.value = PageState.Error
         }
     }
 
